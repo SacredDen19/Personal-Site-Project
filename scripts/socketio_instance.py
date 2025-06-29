c8aabf4a2e74561dbd3b6f0721c7e3d57d52ba2d
sacred <denilsonbej19+github@gmail.com>
2025-06-29 19:16:54 -0400

    Minor change to the instance file

--- scripts/socketio_instance.py
+++ scripts/socketio_instance.py
@@ -1,6 +1,11 @@
+#!/home/sacred/myenv/bin/python3
+
+
+
+
+
 #This file is meant to circumvent the circular log error from importing home.py and snake_handler.py
 #at the same time.
-
 from flask_socketio import SocketIO
 
 socketio = SocketIO()

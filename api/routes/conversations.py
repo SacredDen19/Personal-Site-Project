1de104098d2bbb702c58a982895d6b55685e15b6
SacredDen19 <denilsonbej19+github@gmail.com>
2025-08-10 02:13:35 -0400

    Began the front end revolution for the conversations page. Added dynamic page changes utilizing fetch api. This should hopefully allow me to integrate mySQL into the conversations a bit easier.

--- api/routes/conversations.py
+++ api/routes/conversations.py
@@ -1,4 +1,4 @@
-from flask import sessions, Flask, render_template, request, session, Blueprint, redirect, url_for, Response
+from flask import sessions, Flask, render_template, request, session, Blueprint, redirect, url_for, Response, jsonify
 import sys
 from api.datab import load_loggedin, db_start_connection, register_user, delete_registered
 
@@ -9,7 +9,48 @@ conv_blueprint = Blueprint('conv', __name__)
 def main():
     if request.method == 'GET':
         user = load_loggedin()
-        messages = [
+        conversations = get_conversations()
+        #sorted_messages = sorted(messages, key=lambda x: x['time']) Sorts the messages dictionary using lambda function x which iterates through each message in messages dictionary and sorts them by time
+        return render_template('conversations.html', user=user, conversations=conversations)
+    
+@conv_blueprint.route('/conversations/<conversationID>', methods=['GET'])
+def show_conversation(conversationID):
+
+    conversation_id = get_conversations().get(conversationID)
+
+    #conversations_json = jsonify(get_conversations())
+    return jsonify(conversation_id)
+    #return render_template('conversations.html', user=user, messages=conversations, conversations_json=conversations_json)
+
+
+
+def get_messages():
+
+    messages = [
+        {'conversationID': 0, 'user': 'user2', 'text':"Welcome to the conversation page. More to come.", 'time': "08-06-2025 11:00"},
+        {'conversationID': 1, 'user': 'user1', 'text':"Hello, user2, how are you?", 'time': "08-06-2025 13:00"},
+        {'conversationID': 1, 'user': 'user2', 'text':"Hello user1, I am good. How is it?", 'time': "08-06-2025 13:02"},
+        {'conversationID': 1, 'user': 'user1', 'text':"It is very good. How's the day?", 'time': "08-06-2025 13:10"},
+        {'conversationID': 1, 'user': 'user2', 'text':"It's a nice day.", 'time': "08-06-2025 17:35"},
+        {'conversationID': 1, 'user': 'user2', 'text':"Did you get those documents?", 'time': "08-06-2025 17:35"},
+        {'conversationID': 1, 'user': 'user1', 'text':"I have not got them yet.", 'time': "08-06-2025 17:38"},
+        {'conversationID': 1, 'user': 'user2', 'text':"Office closes early today. Hurry.", 'time': "08-06-2025 17:41"},
+        {'conversationID': 1, 'user': 'user1', 'text':"I will now. Thanks.", 'time': "08-06-2025 17:55"},
+        {'conversationID': 1, 'user': 'user2', 'text':"No problem, don't forget to get your drive", 'time': "08-06-2025 18:01"},
+        {'conversationID': 1, 'user': 'user1', 'text':"Of course, I won't forget.", 'time': "08-06-2025 18:04"},
+        
+    ]
+    sorted_messages = sorted(messages, key=lambda x: x['time']) #Sorts the messages dictionary using lambda function x which iterates through each message in messages dictionary and sorts them by time
+    return sorted_messages
+
+
+def get_conversations():
+    conversations = {
+        '0' : {
+            "messages" : [{'user': 'user2', 'text':"Welcome to the conversation page. More to come.", 'time': "08-06-2025 11:00"}
+            ]},
+        '1' :{ 
+            "messages" : [
             {'user': 'user1', 'text':"Hello, user2, how are you?", 'time': "08-06-2025 13:00"},
             {'user': 'user2', 'text':"Hello user1, I am good. How is it?", 'time': "08-06-2025 13:02"},
             {'user': 'user1', 'text':"It is very good. How's the day?", 'time': "08-06-2025 13:10"},
@@ -19,8 +60,13 @@ def main():
             {'user': 'user2', 'text':"Office closes early today. Hurry.", 'time': "08-06-2025 17:41"},
             {'user': 'user1', 'text':"I will now. Thanks.", 'time': "08-06-2025 17:55"},
             {'user': 'user2', 'text':"No problem, don't forget to get your drive", 'time': "08-06-2025 18:01"},
-            {'user': 'user1', 'text':"Of course, I won't forget.", 'time': "08-06-2025 18:04"},
-
-        ]
-        sorted_messages = sorted(messages, key=lambda x: x['time']) #Sorts the messages dictionary using lambda function x which iterates through each message in messages dictionary and sorts them by time
-        return render_template('conversations.html', user=user, messages=sorted_messages)
+            
+            {'user': 'user1', 'text':"New day, new conversation sample.", 'time': "08-10-2025 00:04"},
+            {'user': 'user2', 'text':"Right, this sample conversation At one point the scroll bar and text wrapping will be put to the test with this message. Do you think so otherwise?", 'time': "08-10-2025 00:14"},
+            {'user': 'user1', 'text':"I try not to be a pessimist about the conversations sample features.", 'time': "08-10-2025 00:18"},
+            {'user': 'user1', 'text':"At one point everything will be fixed and things will be better", 'time': "08-10-2025 00:24"},
+            {'user': 'user2', 'text':"I guess we will see where time takes us.", 'time': "08-10-2025 01:04"},
+        ]},
+        '2' :{},
+    }
+    return conversations
--- static/JS/conversations.js
+++ static/JS/conversations.js
@@ -1,9 +1,83 @@
+console.log("JS ran")
 
+const messagesContainer = document.getElementsByClassName('messages-container');
+const messageCollection = messagesContainer[0].children;
+const previewsContainer = document.getElementsByClassName('conversations-preview');
 //This sets the scroll bar to the bottom when the conversations page loads
-
-function scrollToBottom() {
-    const messagesContainer = document.getElementsByClassName('messages-container')[0];
-    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+function scrollToBottom(index) {
+    messagesContainer[index].scrollTop = messagesContainer[index].scrollHeight;
 }
-window.onload = scrollToBottom;
 
+
+//This function will load the conversations based on the message tabs/previews in the left column
+async function loadConversations(e) { //This asynchronous function is needed to work with fetch()
+    try {
+        //console.log("async worked")
+        const conversationID = e.target.id; //gets id from element that fired the event
+        console.log("Messages container: ", messagesContainer, "Message Container Children Collection: ",messageCollection);
+        console.log("Target preview:", conversationID);
+        
+        const response = await fetch(`/conversations/${conversationID}`) //Creates a request to the backend route
+        history.pushState({ conversationID} , '', `/conversations/${conversationID}`); //Changes the url without reloading the page
+        const data = await response.json();
+
+        //Interates through messageCollection (messages-container) in order the clear the previous conversation
+        for (i=0; i < messageCollection.length;i++) {
+            messagesContainer[i].innerHTML = ''; //Clears any messages-container the previous conversation
+            console.log("cleared Container: with ID of ",i,messagesContainer[i]);
+        }
+        //For every message received, this creates a message box for it and dynamically changes the page to show it/them.
+        for (i=0; i < data.messages.length; i++) {
+            //constant declarations
+            const message = data.messages[i];
+            //console.log("data response message: ", message) prints the message text data objects
+            const messageDiv = document.createElement('div');
+            const usernameSpan = document.createElement('span');
+            const messageTextSpan = document.createElement('span');
+
+            //Class assignment
+            messageDiv.classList.add('message');
+            usernameSpan.classList.add('username');
+            messageTextSpan.classList.add('text');
+
+            if (message.user == "user1") { //Checks if the user is the one that is logged in and assings the correct message box class for styling
+                messageDiv.classList.add('them');
+            } else {
+                messageDiv.classList.add('me');
+            }
+            //Adds the username and text data to the correct html element
+            usernameSpan.innerHTML = `${message.user}`;
+            messageTextSpan.innerHTML = `${message.text}`;
+            //Adds the actual username and text elements to the message box
+            messageDiv.appendChild(usernameSpan);
+            messageDiv.appendChild(messageTextSpan);
+            //Adds the message text data to message box divs 
+            console.log("conversation: ", conversationID);
+            //Check if message collection exists or is empty
+            if (messageCollection.length == undefined  || messageCollection.length == "0" ) {
+                messagesContainer[0].appendChild(messageDiv);
+            }   else {
+                messagesContainer[0].appendChild(messageDiv);
+            }
+        }
+
+
+        console.log(data);
+    } catch (err) {
+        console.error("Error", err);
+    }
+}
+
+//window.onload = scrollToBottom;
+
+if (previewsContainer) { //checks if previews exists or loads
+    for (let x=0; x <= previewsContainer.length; x++) { //iterates through the previewsContainer HTML collection (Array)
+        previewsContainer[x].addEventListener("click", function (e) { //listens for click event in previewsContainer
+
+            loadConversations(e);
+        });
+        console.log("Preview loaded!", previewsContainer[x]);
+    }
+} else {
+    console.log("previews not loaded")
+}

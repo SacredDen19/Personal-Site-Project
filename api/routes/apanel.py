584a366a2068c646d3be802ead8cbe8ca7f47224
SacredDen <denilsonbej19+GitHub@gmail.com>
2025-10-08 14:35:56 -0400

    Added render template for admin panel

--- api/routes/apanel.py
+++ api/routes/apanel.py
@@ -1,5 +1,11 @@
-from flask import Blueprint, Flask, render_template, session
+from flask import Blueprint, Flask, render_template, session, request
+from api.datab import load_loggedin
 
 admin_panel = Blueprint('apanel', __name__)
 
 
+@admin_panel.route('/conversations', methods=['GET'])
+def landing():
+    if request.method == 'GET':
+        usr = load_loggedin
+        return render_template('admin_dashboard.html', user=usr)

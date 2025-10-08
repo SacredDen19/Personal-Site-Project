73087a37173475c481bdb486152d3ef801a56a95
SacredDen <denilsonbej19+GitHub@gmail.com>
2025-10-08 18:21:15 -0400

    Attempt to line parsing 5

--- api/routes/apanel.py
+++ api/routes/apanel.py
@@ -9,9 +9,9 @@ admin_panel = Blueprint('apanel', __name__)
 def landing():
     if request.method == 'GET':
         usr = load_loggedin()
-        
+        lines = {}
         with open("/var/www/public_html/logs/error.log") as log_file:
             for line in log_file:
-                line= log_file.read()
-                parsed_line = re.split("\n+", line)
-        return render_template('admin_dashboard.html', user=usr, line=parsed_line)
+                lines[line]= log_file.readline()
+                #parsed_line = re.split("\n+", line)
+        return render_template('admin_dashboard.html', user=usr, line=lines)
